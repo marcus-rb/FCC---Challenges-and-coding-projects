da0c50dd4672ea55bfe1390ef9b630a8f68e1831
Marcus R B <30393785+marcus-rb@users.noreply.github.com>
2018-11-05 14:45:47 +0100

    Cashier.js - update
    Updated to increase effiency, as well as reducing the amount of lines by about 50%

--- cashier.js
+++ cashier.js
@@ -1,83 +1,51 @@
 function checkCashRegister(price, cash, cid) {
-  //Using cents as base unit for currency, as JS has issues with floating points
-  const urgay = [].concat(...cid);
-  console.log(urgay);
+  let verificationCid = [].concat(...cid);
+  const backupChange = cash-price;
+
+  verificationCid = verificationCid.map((cur,index,arr) => [cur, arr[index+1]]).filter(item => typeof(item[0]) != "number"); //We now have a non-changing copy of cid. This is the beaty of ES6
   
-  cash*=100;
-  price*=100;
+  //JS has issues with floats, so everything from here-on gets multiplied with 100 and parsed to int
 
-  var change = cash - price;
-  let backup = change / 100;
+  const names = cid.map(item => item[0]).reverse();
+  const values = [10000,2000,1000,500,100,25,10,5,1];
+  price = parseInt(price*100); cash = parseInt(cash*100); 
+  let change = parseInt(cash-price);
+  for (let i of cid) i[1] = parseInt(i[1]*100);
   cid = cid.reverse();
-  let totalChange = cid.map(item => item[1]).reduce((a,b) => a+b);
+  
+  //______________________________________________________
 
-  let names = cid.map(item => item[0]);
-  let vals = [10000,2000,1000,500,100,25,10,5,1];
-
-  for (let item of cid) {
-    item[1]*=100;
-  }
-
-  let index = 0;
-  let result = [];
-  let pay = ["",0];
-
-  function returner() {
+  let index = 0; let pay = ["",0]; let result = []; //From here we decide what to give back to the customer, pay will be the amount of each note, result will be the total
+  function changeReturner() {
+    function operation() { result.push(pay); pay = ["",0]; index++; changeReturner() }; //To avoid duplicating code, I define a short method to be executed
     while (index < names.length) {
-      if (change >= vals[index]) {
-        if (cid[index][1] >= vals[index]) {
-          pay[0] = cid[index][0];
-          pay[1] += vals[index];
-          cid[index][1] -= vals[index];
-          change -= vals[index];
-          returner();
-        } else {
-          result.push(pay);
-          pay = ["", 0];
-          index++;
-          returner();
-        }
-      } else {
-        result.push(pay);
-        pay = ["", 0];
-        index++;
-        returner();
-      } 
+      if (change >= values[index]) {
+        if (cid[index][1] >= values[index]) {
+          pay[0] = names[index]; pay[1] += values[index]; 
+          cid[index][1] -= values[index];
+          change -= values[index];
+          changeReturner();
+        } else {operation()}
+      } else {operation()}
     }
   }
 
-  returner();
-
-  result = result.filter(item => item.indexOf(0) == -1);
-
-  for (let item of result) {
-    item[1]/=100;
-  }
-  console.log(result);
-
-  let state = {
+  changeReturner();
+  result = result.filter(item => item[1] != 0);
+  for (let i of result) i[1] /= 100;
+ 
+  let drawer = {
     status: "OPEN",
     change: result
   }
 
-  let returnedChange = result.map(item => item[1]).reduce((a,b) => a+b);
+  if (backupChange > result.map(item => item[1]).reduce((a,b) => a+b)) { //First check if the total amount of cash returned by the changeReturner is less than required
+    drawer.status = "INSUFFICIENT_FUNDS"; drawer.change = [];
+  } else if (JSON.stringify(result) == JSON.stringify(verificationCid.filter(item => item[1] != 0))) { //Then check if it is equal, thus closing the drawer.
+    drawer.status = "CLOSED"; drawer.change = verificationCid; //Here we finally get to use the cid-copy we defined in the beginning.
+  }
 
-  if (change > returnedChange) {
-    state.status = "INSUFFICIENT_FUNDS",
-    state.change = [];
-  }
-  
-  if (backup == totalChange) {
-    state.status = "CLOSED";
-    let newArr = [];
-    for (let i = 0; i <urgay.length; i+=2) {
-      newArr.push([urgay[i],urgay[i+1]]);
-    }
-    state.change = newArr;
-  }
-  console.log(state);
-  // Here is your change, ma'am.
-  return state;
+  return drawer;
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
